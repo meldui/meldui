598241ebb8a0a11e5003ad20e525ab6b6d2d6d78
Dipayan Bhowmick <dipayan@altimate.ai>
2025-11-23 04:24:21 +0530

    Fixed an issue with toggleGroup

--- packages/vue/src/components/ui/toggle-group/ToggleGroupItem.vue
+++ packages/vue/src/components/ui/toggle-group/ToggleGroupItem.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
-import { reactiveOmit } from "@vueuse/core";
-import type { VariantProps } from "class-variance-authority";
-import type { ToggleGroupItemProps } from "reka-ui";
-import { ToggleGroupItem, useForwardProps } from "reka-ui";
-import type { HTMLAttributes } from "vue";
-import { inject } from "vue";
-import { toggleVariants } from "@/components/ui/toggle";
-import { cn } from "@/lib/utils";
+import { reactiveOmit } from '@vueuse/core'
+import type { VariantProps } from 'class-variance-authority'
+import type { ToggleGroupItemProps } from 'reka-ui'
+import { ToggleGroupItem, useForwardProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { inject } from 'vue'
+import type { toggleVariants } from '@/components/ui/toggle'
+import { cn } from '@/lib/utils'
 
 type ToggleGroupVariants = VariantProps<typeof toggleVariants> & {
-    spacing?: number;
-};
+  spacing?: number
+}
 
 const props = defineProps<
-    ToggleGroupItemProps & {
-        class?: HTMLAttributes["class"];
-        variant?: ToggleGroupVariants["variant"];
-        size?: ToggleGroupVariants["size"];
-    }
->();
+  ToggleGroupItemProps & {
+    class?: HTMLAttributes['class']
+    variant?: ToggleGroupVariants['variant']
+    size?: ToggleGroupVariants['size']
+  }
+>()
 
-const context = inject<ToggleGroupVariants>("toggleGroup");
+const context = inject<ToggleGroupVariants>('toggleGroup')
 
-const delegatedProps = reactiveOmit(props, "class", "size", "variant");
-const forwardedProps = useForwardProps(delegatedProps) as ToggleGroupItemProps;
+const delegatedProps = reactiveOmit(props, 'class', 'size', 'variant')
+const forwardedProps = useForwardProps(delegatedProps) as ToggleGroupItemProps
 </script>
 
 <template>
